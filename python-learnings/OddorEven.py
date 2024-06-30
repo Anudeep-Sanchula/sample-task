5843b968f22aede3463d93b99b3dadda12dcf2ab
Anudeep-Sanchula <anudeepssv@gmail.com>
2024-06-30 10:47:16 -0500

    quick fix and adding new file

--- python-learnings/OddorEven.py
+++ python-learnings/OddorEven.py
@@ -1,6 +1,6 @@
-num = int(input(Enter some random number ))
+num = int(input("Enter some random number: "))
 remainder = num%2
 if (remainder == 0):
-    print('Given number', num, is Even)
+    print('Given number', num, "is Even")
 else:
-    print('Given number', num, is Odd)
+    print('Given number', num, "is Odd")
